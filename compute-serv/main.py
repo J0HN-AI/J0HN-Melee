dddef5b0562766b74e4adfc69ba12f46251cc918
Constructor-999 <j_durrer@hotmail.com>
2025-06-11 13:00:17 +0200

    Brain main.py file cleanup

--- compute-serv/main.py
+++ compute-serv/main.py
@@ -1,10 +1,11 @@
-import tomli
 from pyfiglet import Figlet
+import threading as th
+from halo import Halo
+
 import pathlib
 import socket
 import struct
-import threading as th
-from halo import Halo
+import tomli
 
 def get_stage(stage):
         match stage:
@@ -75,7 +76,7 @@ def get_player(hex_player):
                 return 0x18
 
 def client_2_loopback_router(loopback_socket:socket.socket, client_socket:socket.socket, stop:th.Event, n_projectiles:int):
-    payload_char = "iffffiiff??ii??iiii??hhhhhhhh??ffffffff??ffffffffffffffffh" + "ffffhhhh"*n_projectiles
+    payload_char = "l?ffffiiff??ii??iiii??hhhhhhhh??ffffffff??ffffffffffffffffh" + "ffffhhhh"*n_projectiles
     payload_size = struct.calcsize(payload_char)
 
     while not stop.is_set():
@@ -84,8 +85,7 @@ def client_2_loopback_router(loopback_socket:socket.socket, client_socket:socket
     client_socket.close()
 
 def loopback_2_client_router(loopback_socket:socket.socket, client_socket:socket.socket, stop:th.Event, n_projectiles:int):
-    #payload_char = "iffffiiff??ii??iiii??hhhhhhhh??ffffffff??ffffffffffffffffh" + "ffffhhhh"*n_projectiles
-    payload_char = "i"
+    payload_char = "i???????????ffffff"
     payload_size = struct.calcsize(payload_char)
     
     while not stop.is_set():
@@ -93,29 +93,22 @@ def loopback_2_client_router(loopback_socket:socket.socket, client_socket:socket
         client_socket.send(payload)
     loopback_socket.close()
 
-if __name__ == "__main__":
-    print(Figlet(font="larry3d").renderText("J0HN Melee"))
-    print()
-
-    config = tomli.load(open(f"{pathlib.Path(__file__).parent.resolve()}/config.toml", "rb"))
-
-    network_config = config["network-config"]
-    instances_configuration = config["instances"]
-    nb_projectiles = config["training-config"]["n_projectiles"]
-    nb_instances = len(instances_configuration)
-
+def setup_rooting_table(config:dict):
     routing_table = {}
-    socket_table = {}
-    stop_routers = th.Event()
-
     for i, instance in instances_configuration.items():
         routing_table[config["network-config"]["envs_base_port"] + int(i)] = instance["ip"]
+    
+    return routing_table
 
-
+def setup_loopback_int(network_config:dict, nb_instances:int):
     loopback_int_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     loopback_int_socket.bind(("127.0.0.1", network_config["router_port"]))
     loopback_int_socket.listen(nb_instances)
-    
+
+    return loopback_int_socket
+
+def setup_loopback_connections(loopback_int_socket: socket.socket, routing_table: dict, nb_instances:int):
+    socket_table = {}
     try:
         loopback_spinner = Halo(f"Waiting for loopback instances 0/{nb_instances}", spinner="dots")
         loopback_spinner.start()
@@ -130,12 +123,17 @@ if __name__ == "__main__":
         loopback_spinner.succeed("All instances loopback connected ~(^-^)~")
     except KeyboardInterrupt:
             loopback_spinner.fail(f"Only {loopback_instances_connected} loopback instance/s are connected (╯°□°）╯︵ ┻━┻")
-    
 
+    return socket_table
+
+def setup_client_int(network_config:dict, nb_instances:int):
     client_int_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_int_socket.bind((network_config["HPC_ip"], network_config["HPC_port"]))
     client_int_socket.listen(nb_instances)
+    
+    return client_int_socket
 
+def setup_client_connections(client_int_socket:socket.socket, socket_table:dict, stop_routers:th.Event, nb_projectiles:int, nb_instances:int):
     try:
         clients_spinner = Halo(f"Waiting for client instances 0/{nb_instances}", spinner="dots")
         clients_spinner.start()
@@ -152,6 +150,27 @@ if __name__ == "__main__":
         clients_spinner.succeed("All client instances connected ~(^-^)~")
     except KeyboardInterrupt:
             clients_spinner.fail(f"Only {clients_instances_connected} client instance/s are connected (╯°□°）╯︵ ┻━┻")
+
+if __name__ == "__main__":
+    print(Figlet(font="larry3d").renderText("J0HN Melee"))
+    print()
+
+    config = tomli.load(open(f"{pathlib.Path(__file__).parent.resolve()}/config.toml", "rb"))
+
+    network_config = config["network-config"]
+    instances_configuration = config["instances"]
+    nb_projectiles = config["training-config"]["n_projectiles"]
+    nb_instances = len(instances_configuration)
+
+    stop_routers = th.Event()
+
+    routing_table = setup_rooting_table(config)
+
+    loopback_int_socket = setup_loopback_int(network_config, nb_instances)
+    socket_table = setup_loopback_connections(loopback_int_socket, routing_table, nb_instances)
+
+    client_int_socket = setup_client_int(network_config, nb_instances)
+    setup_client_connections(client_int_socket, socket_table, stop_routers, nb_projectiles, nb_instances)
 
     try:
         print("Router Test is UP")
