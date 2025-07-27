914a391d21ed95f5a311d4becea1f25fdb535f54
Constructor-999 <j_durrer@hotmail.com>
2025-07-27 14:16:39 +0200

    Added interface binding retry

--- compute-serv/main.py
+++ compute-serv/main.py
@@ -1,14 +1,25 @@
 from pyfiglet import Figlet
 import threading as th
 from halo import Halo
-from flask import Flask
-from flask_socketio import SocketIO
+import multiprocessing as mp
 
 import pathlib
 import socket
 import struct
 import tomli
-import eventlet
+import os
+import time
+
+class tcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def get_stage(hex_stage):
     match hex_stage:
@@ -82,31 +93,33 @@ def client_2_loopback_router(loopback_socket:socket.socket, client_socket:socket
     payload_char = "l?ffffiiff??ii??iiii??iiiiiiii??ffffffff??ffffffffffffffffi" + "ffffiiii"*n_projectiles
     payload_size = struct.calcsize(payload_char)
 
-    while not stop.is_set():
-        payload = client_socket.recv(payload_size)
-        loopback_socket.send(payload)
-    client_socket.close()
+    try:
+        while not stop.is_set():
+            payload = client_socket.recv(payload_size)
+            loopback_socket.send(payload)
+    except OSError as e:
+        client_socket.close()
 
 def loopback_2_client_router(loopback_socket:socket.socket, client_socket:socket.socket, stop:th.Event):
     payload_char = "iiiiiiiiiiffffff"
     payload_size = struct.calcsize(payload_char)
     
-    while not stop.is_set():
-        payload = loopback_socket.recv(payload_size)
-        client_socket.send(payload)
-    loopback_socket.close()
-
-def loopback_2_frontend(loopback_socket:socket.socket, websocket:SocketIO, int_addr:socket._RetAddress, stop:th.Event):
+    try:
+        while not stop.is_set():
+            payload = loopback_socket.recv(payload_size)
+            client_socket.send(payload)
+    except OSError as e:
+        loopback_socket.close()
+    
+def loopback_2_logs(loopback_socket:socket.socket, logs_dict, int_addr, stop, lock):
     payload_char = "fiiiiiiiiffi"
     payload_size = struct.calcsize(payload_char)
 
     while not stop.is_set():
         log_payload = loopback_socket.recv(payload_size)
         log = struct.unpack(payload_char, log_payload)
-
-        websocket.emit("new_log", {
-            "instance": str(int_addr[1]),
-            "logs": {
+        instance_log = {
+            str(int_addr[1]): {
                 "game": log[8],
                 "game_time": round(log[0], 3),
                 "stage": get_stage(log[1]),
@@ -120,7 +133,21 @@ def loopback_2_frontend(loopback_socket:socket.socket, websocket:SocketIO, int_a
                 "avg_score": round(log[10], 5),
                 "learn_iters": log[11]
                 }
-        })
+        }
+
+        with lock:
+            logs_dict.update(instance_log)
+
+def logs_2_terminal(logs_dict, lock, nb_instances:int):
+    logs = {}
+    old_logs = {}
+
+    with lock:
+        logs = dict(logs_dict)
+    
+    if old_logs != logs:
+        print(logs)
+        old_logs = logs
 
 def setup_rooting_table(config:dict):
     routing_table = {}
@@ -130,16 +157,19 @@ def setup_rooting_table(config:dict):
     return routing_table
 
 def setup_loopback_int(network_config:dict, nb_instances:int):
-    loopback_int_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    loopback_int_socket.bind(("127.0.0.1", network_config["router_port"]))
-    loopback_int_socket.listen(nb_instances)
+    try:
+        loopback_int_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        loopback_int_socket.bind(("127.0.0.1", network_config["router_port"]))
+        loopback_int_socket.listen(nb_instances)
 
-    return loopback_int_socket
+        return loopback_int_socket
+    except OSError as e:
+        print(e)
 
 def setup_loopback_connections(loopback_int_socket: socket.socket, routing_table: dict, nb_instances:int):
     socket_table = {}
     try:
-        loopback_spinner = Halo(f"Waiting for loopback instances 0/{nb_instances}", spinner="dots")
+        loopback_spinner = Halo(f"{tcolors.WARNING}Waiting for loopback instances 0/{nb_instances}{tcolors.ENDC}", spinner="dots")
         loopback_spinner.start()
         loopback_instances_connected = 0
 
@@ -147,24 +177,27 @@ def setup_loopback_connections(loopback_int_socket: socket.socket, routing_table
             loopback, addr = loopback_int_socket.accept()
             socket_table[routing_table.get(addr[1])] = loopback
             loopback_instances_connected += 1
-            loopback_spinner.text = f"Waiting for loopback instances {loopback_instances_connected}/{nb_instances}"
+            loopback_spinner.text = f"{tcolors.WARNING}Waiting for loopback instances {loopback_instances_connected}/{nb_instances}{tcolors.ENDC}"
 
-        loopback_spinner.succeed("All loopback instances are connected ~(^-^)~")
+        loopback_spinner.succeed(f"{tcolors.OKGREEN}All loopback instances are connected ~(^-^)~{tcolors.ENDC}")
     except KeyboardInterrupt:
-            loopback_spinner.fail(f"Only {loopback_instances_connected} loopback instance/s are connected (╯°□°）╯︵ ┻━┻")
+            loopback_spinner.fail(f"{tcolors.FAIL}Only {loopback_instances_connected} loopback instance/s are connected (╯°□°）╯︵ ┻━┻{tcolors.ENDC}")
     
     return socket_table
 
 def setup_client_int(network_config:dict, nb_instances:int):
-    client_int_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_int_socket.bind((network_config["HPC_ip"], network_config["HPC_port"]))
-    client_int_socket.listen(nb_instances)
-    
-    return client_int_socket
+    try:
+        client_int_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_int_socket.bind((network_config["HPC_ip"], network_config["HPC_port"]))
+        client_int_socket.listen(nb_instances)
+        
+        return client_int_socket
+    except OSError as e:
+        print(e)
 
 def setup_client_connections(client_int_socket:socket.socket, socket_table:dict, stop_routers:th.Event, nb_projectiles:int, nb_instances:int):
     try:
-        clients_spinner = Halo(f"Waiting for client instances 0/{nb_instances}", spinner="dots")
+        clients_spinner = Halo(f"{tcolors.WARNING}Waiting for client instances 0/{nb_instances}{tcolors.ENDC}", spinner="dots")
         clients_spinner.start()
         clients_instances_connected = 0
 
@@ -174,35 +207,38 @@ def setup_client_connections(client_int_socket:socket.socket, socket_table:dict,
             th.Thread(target=loopback_2_client_router, args=(socket_table.get(addr[0]), client, stop_routers)).start()
 
             clients_instances_connected += 1
-            clients_spinner.text = f"Waiting for client instances {clients_instances_connected}/{nb_instances}"
+            clients_spinner.text = f"{tcolors.WARNING}Waiting for client instances {clients_instances_connected}/{nb_instances}{tcolors.ENDC}"
 
-        clients_spinner.succeed("All client instances are connected ~(^-^)~")
+        clients_spinner.succeed(f"{tcolors.OKGREEN}All client instances are connected ~(^-^)~{tcolors.ENDC}")
     except KeyboardInterrupt:
-            clients_spinner.fail(f"Only {clients_instances_connected} client instance/s are connected (╯°□°）╯︵ ┻━┻")
+            clients_spinner.fail(f"{tcolors.FAIL}Only {clients_instances_connected} client instance/s are connected (╯°□°）╯︵ ┻━┻{tcolors.ENDC}")
 
 def setup_logger_int(network_config:dict, nb_instances:int):
-    logger_int_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    logger_int_socket.bind(("127.0.0.1", network_config["logger_port"]))
-    logger_int_socket.listen(nb_instances)
-
-    return logger_int_socket
-
-def setup_logger_connections(logger_int_socket: socket.socket, websocket: SocketIO, stop_logger: th.Event, nb_instances:int):
     try:
-        logger_spinner = Halo(f"Waiting for loopback instances 0/{nb_instances}", spinner="dots")
+        logger_int_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        logger_int_socket.bind(("127.0.0.1", network_config["logger_port"]))
+        logger_int_socket.listen(nb_instances)
+
+        return logger_int_socket
+    except OSError as e:
+        print(e)
+
+def setup_logger_connections(logger_int_socket: socket.socket, logs_dict, stop_logger, lock, nb_instances:int):
+    try:
+        logger_spinner = Halo(f"{tcolors.WARNING}Waiting for loopback instances 0/{nb_instances}{tcolors.ENDC}", spinner="dots")
         logger_spinner.start()
         logger_instances_connected = 0
 
         while logger_instances_connected != nb_instances:
             loopback, addr = logger_int_socket.accept()
-            th.Thread(target=loopback_2_frontend, args=(loopback, websocket, addr, stop_logger)).start()
+            th.Thread(target=loopback_2_logs, args=(loopback, logs_dict, addr, stop_logger, lock)).start()
             logger_instances_connected += 1
-            logger_spinner.text = f"Waiting for loopback instances {logger_instances_connected}/{nb_instances}"
+            logger_spinner.text = f"{tcolors.WARNING}Waiting for loopback instances {logger_instances_connected}/{nb_instances}{tcolors.ENDC}"
 
-        logger_spinner.succeed("All loopback instances are connected ~(^-^)~")
+        logger_spinner.succeed(f"{tcolors.OKGREEN}All loopback instances are connected ~(^-^)~{tcolors.ENDC}")
     except KeyboardInterrupt:
-            logger_spinner.fail(f"Only {logger_instances_connected} loopback instance/s are connected (╯°□°）╯︵ ┻━┻")
-    
+            logger_spinner.fail(f"{tcolors.FAIL}Only {logger_instances_connected} loopback instance/s are connected (╯°□°）╯︵ ┻━┻{tcolors.ENDC}")
+
 if __name__ == "__main__":
     print(Figlet(font="larry3d").renderText("J0HN Melee"))
     print()
@@ -216,9 +252,10 @@ if __name__ == "__main__":
 
     stop_routers = th.Event()
 
-    eventlet.monkey_patch()
-    app = Flask(__name__)
-    socketio = SocketIO(app, cors_allowed_origins="*")
+    stop_loggers = mp.Event()
+    loggers_manager = mp.Manager()
+    logs_dict = loggers_manager.dict()
+    logs_lock = loggers_manager.Lock()
 
     routing_table = setup_rooting_table(config)
 
@@ -226,15 +263,19 @@ if __name__ == "__main__":
     socket_table = setup_loopback_connections(loopback_int_socket, routing_table, nb_instances)
 
     logger_int_socket = setup_logger_int(network_config, nb_instances)
-    setup_logger_connections(logger_int_socket, socketio, stop_routers, nb_instances)
+    setup_logger_connections(logger_int_socket, logs_dict, stop_loggers, logs_lock, nb_instances)
     
     client_int_socket = setup_client_int(network_config, nb_instances)
     setup_client_connections(client_int_socket, socket_table, stop_routers, nb_projectiles, nb_instances)
     
-    try:
-        socketio.run(app, host="0.0.0.0", port=network_config["logger_websocket_port"])
-    except KeyboardInterrupt:
-        stop_routers.set()
-        client_int_socket.close()
-        loopback_int_socket.close()
-        logger_int_socket.close()
+    while True:
+        try:
+            logs_2_terminal(logs_dict, logs_lock, nb_instances)
+        except KeyboardInterrupt:
+            stop_routers.set()
+            stop_loggers.set()
+            client_int_socket.close()
+            loopback_int_socket.close()
+            logger_int_socket.close()
+            del logs_lock
+            break
